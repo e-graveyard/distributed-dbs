4925466f5e383b036ba502302e1666eacba1c94b
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-10-24 23:04:48 -0300

    Implementa classe com utilitários

---
+++ components/server/src/main/java/Utils.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.util.Random;
+
+public class Utils
+{
+    private static Random rand = new Random();
+
+    private static final int MAX_PORT_NUMBER = 65535;
+    private static final int MIN_PORT_NUMBER = 5000;
+
+    public static String generateUniqueName()
+    {
+        String[] humanNames = {
+            "JAMES", "JOHN", "ROBERT", "MICHAEL", "WILLIAM",
+            "DAVID", "RICHARD", "CHALES", "JOSEPH", "THOMAS"
+        };
+
+        int index = rand.nextInt(humanNames.length);
+
+        String name = humanNames[index];
+        String numb = Integer.toString(rand.nextInt(1000000));
+
+        String uniqueName = name + "-" + numb;
+
+        return (name + "-" + numb);
+    }
+
+    public static int generateValidPort()
+    {
+        int port = rand.nextInt((MAX_PORT_NUMBER - MIN_PORT_NUMBER) + 1) - MIN_PORT_NUMBER;
+
+        return port;
+    }
+}
