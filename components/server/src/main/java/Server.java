f33e29ea00068e343483e48090c2ba7e5042c600
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-10-31 21:54:27 -0300

    Implementa método de escuta e estabelecimento de conexão

--- components/server/src/main/java/Server.java
+++ components/server/src/main/java/Server.java
@@ -1,14 +1,24 @@
 package main;
 
+import java.io.IOException;
+
+import java.net.Socket;
+import java.net.ServerSocket;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Server
 {
-    private String name;
     private int port;
+    private String name;
+    private List<Socket> connections;
 
     public Server()
     {
-        this.name = Utils.generateUniqueName();
         this.port = Utils.generateValidPort();
+        this.name = Utils.generateUniqueName();
+        this.connections = new ArrayList<>();
     }
 
     public String getName()
@@ -19,5 +29,23 @@ public class Server
     public int getPort()
     {
         return this.port;
+    }
+
+    public void listen() throws IOException
+    {
+        try(ServerSocket socket = new ServerSocket(this.port))
+        {
+            while(true)
+            {
+                Socket client = socket.accept();
+                System.out.println("New connection established.");
+
+                this.connections.add(client);
+                Connection c = new Connection(client, this);
+                new Thread(c).start();
+            }
+
+        }
+
     }
 }
