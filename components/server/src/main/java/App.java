8320b052c9592e816f56fab391374631ea8b6c19
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-10-31 22:14:56 -0300

    Inclui tratamento de erro na função principal

--- components/server/src/main/java/App.java
+++ components/server/src/main/java/App.java
@@ -1,19 +1,26 @@
 package main;
 
+import java.io.IOException;
+
 public class App
 {
 	public static void main( String[] args )
 	{
         Server s = new Server();
 
-        String message = "\n\nHi there. I'm server @name. Listening on port @port...";
+        try
+        {
+            String message = "\n\nHi there. I'm server @name. Listening on port @port..."
+                .replace("@name", s.getName())
+                .replace("@port", Integer.toString(s.getPort()));
+            System.out.println(message);
 
-        message = message
-            .replace("@name", s.getName())
-            .replace("@port", Integer.toString(s.getPort()));
-
-        System.out.println(message);
-
-        s.listen();
+            s.listen();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Something went wrong...\n");
+            e.printStackTrace();
+        }
 	}
 }
