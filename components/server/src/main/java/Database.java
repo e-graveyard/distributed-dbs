6694e3f3c7be9f2936f9b012ef2f7ed652eab54b
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-02 04:54:27 -0300

    Inclui esboço não implementado da classe de conexão com banco

---
+++ components/server/src/main/java/Database.java
@@ -0,0 +1,24 @@
+package main;
+
+class Database
+{
+    public void create(Book book)
+    {
+
+    }
+
+    public void read(int id)
+    {
+
+    }
+
+    public void update(Book book)
+    {
+
+    }
+
+    public void delete(int id)
+    {
+
+    }
+}
