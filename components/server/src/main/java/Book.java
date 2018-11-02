36f27709d5680b9434a7aa5bbb75339e363f0cff
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-02 04:51:18 -0300

    Inclui getters no POJO dos livros

--- components/server/src/main/java/Book.java
+++ components/server/src/main/java/Book.java
@@ -20,4 +20,29 @@ public class Book
         this.isbn        = isbn;
         this.pages       = pages;
     }
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public String getAuthor()
+    {
+        return this.author;
+    }
+
+    public String getPublication()
+    {
+        return this.publication;
+    }
+
+    public String getIsbn()
+    {
+        return this.isbn;
+    }
+
+    public int getPages()
+    {
+        return this.pages;
+    }
 }
