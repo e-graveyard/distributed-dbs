de99ea4690767ee71576b77b2e63307af1cce42e
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-05 13:20:57 -0200

    Implementa classe estática de configuração do banco de dados

---
+++ components/server/src/main/java/Config.java
@@ -0,0 +1,10 @@
+package main;
+
+class Config
+{
+    protected static final int PORT        = Integer.parseInt(System.getenv("H2_PORT"));
+    protected static final String BASE     = System.getenv("H2_BASE");
+    protected static final String HOSTNAME = System.getenv("H2_HOSTNAME");
+    protected static final String USERNAME = System.getenv("H2_USERNAME");
+    protected static final String PASSWORD = System.getenv("H2_PASSWORD");
+}
