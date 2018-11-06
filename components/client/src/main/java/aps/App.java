512ba732cd9ca41254f5d81978667a486cae0c2f
Diogo Casagrande <casagrande.diogo@gmail.com>
2018-11-06 21:38:10 -0200

    add connection cliente/controller

--- components/client/src/main/java/aps/App.java
+++ components/client/src/main/java/aps/App.java
@@ -1,18 +1,25 @@
-package com;
+package aps;
 
-import java.util.*;
-/**
- * Hello world!
- *
- */
+import java.net.*;
+
 public class App
 {
     public static void main( String[] args )
     {
-	    if(!args.equals("")){
-		    for(String arg : args)
-			    System.out.println(arg);
-	    }
-        System.out.println( "Hello World!" );
+        //connecção com o controller
+        try{
+            System.out.println("........Fazendo connecção ");
+            Socket clientSocket = new Socket("127.0.0.1", 9999);
+        }catch(Exception e){
+            System.out.println("........Não foi possivel efetuar a conecção Cliente/Controller");
+            System.out.println(e.getMessage());
+        }
+
+        //Envio da mensagem
+        try{
+
+        }catch(Exception e){
+
+        }
     }
 }
