489f859dcb33c6ccb2b0930674f4f7b27c08e488
Diogo Casagrande <casagrande.diogo@gmail.com>
2018-11-06 23:00:57 -0200

    estrutura de troca de mensagem client/controller

--- components/client/src/main/java/aps/App.java
+++ components/client/src/main/java/aps/App.java
@@ -1,22 +1,31 @@
 package aps;
 
+import java.io.OutputStream;
 import java.net.*;
+import java.util.Scanner;
 
 public class App
 {
     public static void main( String[] args )
     {
+        Socket clientSocket = null;
         //connecção com o controller
         try{
             System.out.println("........Fazendo connecção ");
-            Socket clientSocket = new Socket("127.0.0.1", 9999);
+            clientSocket = new Socket("127.0.0.1", 9999);
         }catch(Exception e){
             System.out.println("........Não foi possivel efetuar a conecção Cliente/Controller");
             System.out.println(e.getMessage());
         }
 
         //Envio da mensagem
+        System.out.println("........Envio da mensagem client/controller");
         try{
+            Scanner in = new Scanner(System.in);
+            String menssage = in.nextLine();
+            OutputStream out = clientSocket.getOutputStream();
+
+            out.write(menssage.getBytes());
 
         }catch(Exception e){
 
