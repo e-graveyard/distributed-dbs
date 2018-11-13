ed581383488638b662547f7e7d3a280c57f63093
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-13 04:35:51 -0200

    Corrige referências das variáveis de ambiente

--- components/server/src/main/java/db/Config.java
+++ components/server/src/main/java/db/Config.java
@@ -2,8 +2,8 @@ package main;
 
 class Config
 {
-    protected static final int PORT        = Integer.parseInt(System.getenv("H2_PORT"));
-    protected static final String BASE     = System.getenv("H2_BASE");
+    protected static final int PORT        = Integer.parseInt(System.getenv("H2_TCP_PORT"));
+    protected static final String BASE     = System.getenv("H2_DBNAME");
     protected static final String HOSTNAME = System.getenv("H2_HOSTNAME");
     protected static final String USERNAME = System.getenv("H2_USERNAME");
     protected static final String PASSWORD = System.getenv("H2_PASSWORD");
