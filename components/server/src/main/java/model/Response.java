9e29b8a14c2dbb3b4699af1f9f22c8c6fdea0d1b
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-14 01:42:46 -0200

    Altera escopo da classe, implementa nova estrutura de resposta

---
+++ components/server/src/main/java/model/Response.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.util.HashMap;
+
+class Response
+{
+    private String kind;
+    private boolean success;
+    private HashMap<String, String> envelope;
+    private HashMap<String, String> data;
+
+    public Response()
+    {
+        this.kind = "Response";
+        this.data = new HashMap<>();
+        this.envelope = new HashMap<>();
+    }
+
+    public void setSuccess(boolean success)
+    {
+        this.success = success;
+    }
+
+    public void setEnvelope(String sender, String recipient)
+    {
+        this.envelope.put("from", sender);
+        this.envelope.put("to", recipient);
+    }
+
+    public void putData(String key, String value)
+    {
+        this.data.put(key, value);
+    }
+}
