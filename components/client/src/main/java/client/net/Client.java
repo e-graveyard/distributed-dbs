6779d8d355a4246cd80b5ae9f374f73566593963
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-16 02:21:32 -0200

    Implementa esboço da classe de cliente e fluxo com o usuário

--- components/client/src/main/java/client/net/Client.java
+++ components/client/src/main/java/client/net/Client.java
@@ -5,10 +5,91 @@ package client;
    Julia G. C. Chiba (@JuliaChiba)      - 20511823
  */
 
+import java.util.Random;
+import java.io.OutputStream;
+import java.net.*;
+
 class Client
 {
-    public Client()
-    {
+    private String name;
+    private Random rand;
+    private int controllerPort;
 
+    public Client(int controllerPort)
+    {
+        this.rand = new Random();
+        this.name = this.generateUniqueName();
+        this.controllerPort = controllerPort;
+    }
+
+    private String generateUniqueName()
+    {
+        String[] humanNames = {
+            "JAMES", "JOHN", "ROBERT", "MICHAEL", "WILLIAM",
+            "DAVID", "RICHARD", "CHARLES", "JOSEPH", "THOMAS",
+            "MARY", "LINDA", "ELIZABETH", "SUSAN", "MARGARETH",
+            "DOROTHY", "LISA", "NANCY", "DONNA", "MICHELLE"
+        };
+
+        int index = this.rand.nextInt(humanNames.length);
+
+        String name = humanNames[index];
+        String numb = Integer.toString(this.rand.nextInt(1000000));
+
+        return (name + "-" + numb);
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void act() throws Exception
+    {
+        Runtime.getRuntime().addShutdownHook(
+                new Thread()
+                {
+                   @Override
+                   public void run()
+                   {
+                       System.out.println("");
+                       Logger.warning("SIGINT caught.");
+                       Logger.info("Exiting...");
+
+                       return;
+                   }
+                });
+
+        boolean persist = true;
+        while(persist)
+        {
+            Interface.drawMenu();
+            int option = Interface.getOption();
+            if (option < 0 || option > 4)
+            {
+                Logger.error("bla");
+                continue;
+            }
+
+            switch(option)
+            {
+                case 0:
+                    persist = false;
+                    break;
+
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+            }
+
+        }
     }
 }
