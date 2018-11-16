82f91d96151995a66cb9f2a6520a890bc5859321
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-16 02:52:06 -0200

    Inclui modelo POJO do livro

---
+++ components/client/src/main/java/client/model/Book.java
@@ -0,0 +1,53 @@
+package server;
+/*
+   Caian R. Ertl     (@caianrais)       - 20733152
+   Diogo Casagrande  (@DiogoCasagrande) - 20718678
+   Julia G. C. Chiba (@JuliaChiba)      - 20511823
+ */
+
+class Book
+{
+    private String title;
+    private String author;
+    private String publication;
+    private String isbn;
+    private int pages;
+
+    public Book(String title,
+            String author,
+            String publication,
+            String isbn,
+            int pages)
+    {
+        this.title       = title;
+        this.author      = author;
+        this.publication = publication;
+        this.isbn        = isbn;
+        this.pages       = pages;
+    }
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public String getAuthor()
+    {
+        return this.author;
+    }
+
+    public String getPublication()
+    {
+        return this.publication;
+    }
+
+    public String getIsbn()
+    {
+        return this.isbn;
+    }
+
+    public int getPages()
+    {
+        return this.pages;
+    }
+}
