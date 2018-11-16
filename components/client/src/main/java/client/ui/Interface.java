701ee93348569d2a450a61f97d32021516ee0215
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-16 04:15:04 -0200

    Implementa método de obtenção do objeto de um livro

--- components/client/src/main/java/client/ui/Interface.java
+++ components/client/src/main/java/client/ui/Interface.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.util.Scanner;
+import java.lang.RuntimeException;
 
 class Interface
 {
@@ -25,5 +26,62 @@ class Interface
             option = Integer.parseInt(value);
 
         return option;
+    }
+
+    public static Book getBook()
+    {
+        String title, author, publication, isbn, pages;
+
+        Logger.info("Input the book information:\n");
+
+        try
+        {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.print("\tTitle: ");
+            title = scan.nextLine();
+            if(Input.isEmpty(title))
+                throw new RuntimeException();
+
+            System.out.print("\tAuthor: ");
+            author = scan.nextLine();
+            if(Input.isEmpty(author))
+                throw new RuntimeException();
+
+            System.out.print("\tPublication: ");
+            publication = scan.nextLine();
+            if(Input.isEmpty(publication))
+                throw new RuntimeException();
+
+            System.out.print("\tISBN: ");
+            isbn = scan.nextLine();
+            if(Input.isEmpty(isbn))
+                throw new RuntimeException();
+
+            System.out.print("\tPages: ");
+            pages = scan.nextLine();
+            if(Input.isEmpty(pages))
+                throw new RuntimeException();
+
+            int p = Integer.parseInt(pages);
+
+            System.out.print("\n");
+
+            return new Book(title, author, publication, isbn, p);
+        }
+        catch(NumberFormatException e)
+        {
+            System.out.print("\n");
+            Logger.error("Invalid input.");
+
+            return null;
+        }
+        catch(RuntimeException e)
+        {
+            System.out.print("\n");
+            Logger.error("Empty input.");
+
+            return null;
+        }
     }
 }
