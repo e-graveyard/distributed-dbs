b67c0b415b971030e9e4f03abf1e41d4b6853ef1
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-15 22:54:33 -0200

    Altera namespace, inclui alterações do Diogo (2)

---
+++ components/controller/src/main/java/controller/App.java
@@ -0,0 +1,69 @@
+package controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App extends Thread {
+    String message = "";
+    //conecção com cliente
+    Socket socket = null;
+    ServerSocket server;
+
+
+    private int serveport;
+
+    public static void main( String[] args ) throws IOException {
+
+
+        App appA = new App(9999);
+        appA.start();
+    }
+
+    public App(int port){
+        serveport = port;
+    }
+
+    @Override
+    public void run () {
+
+        do {
+            System.out.println("........Iniciando aplicação Controller!");
+            try {
+                server = new ServerSocket(serveport);
+                System.out.println("........Connectando ao cliente!!!");
+
+                socket = server.accept();
+                System.out.println("........Conecção efetuada com sucesso cliente/controller");
+            } catch (Exception e) {
+                System.out.println("........Não foi possivel conectar ao cliente");
+                System.out.println(e.getMessage());
+            }
+            InputStream inputStream;
+            Scanner scanner;
+            System.out.println("........recebendo dados client/controller");
+            try {
+
+                inputStream = socket.getInputStream();
+                scanner = new Scanner(inputStream);
+                message = scanner.nextLine();
+
+                System.out.println("........Mensagem recebida: " + message);
+            } catch (Exception e) {
+            }
+
+            try {
+                server.close();
+                socket.close();
+            } catch (Exception e) {
+                System.out.println("Erro ao encerrar a comunicaÃ§Ã£o.");
+                System.out.println(e.getMessage());
+            }
+        } while (!message.equals("0"));
+
+    }
+}
