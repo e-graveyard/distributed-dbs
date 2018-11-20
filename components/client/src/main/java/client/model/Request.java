d4f593665acea516388935d81065b4341d559e1c
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-20 03:55:53 -0200

    Implementa modelo da requisição

--- components/client/src/main/java/client/model/Request.java
+++ components/client/src/main/java/client/model/Request.java
@@ -5,10 +5,32 @@ package client;
    Julia G. C. Chiba (@JuliaChiba)      - 20511823
  */
 
+import java.util.HashMap;
+
 class Request
 {
+    private String kind;
+    private HashMap<String, String> envelope;
+    private HashMap<String, String> data;
+
     public Request()
     {
+        this.data = new HashMap<>();
+        this.envelope = new HashMap<>();
+    }
 
+    public void setKind(String kind)
+    {
+        this.kind = kind;
+    }
+
+    public void setSender(String sender)
+    {
+        this.envelope.put("from", sender);
+    }
+
+    public void putData(String key, String value)
+    {
+        this.data.put(key, value);
     }
 }
