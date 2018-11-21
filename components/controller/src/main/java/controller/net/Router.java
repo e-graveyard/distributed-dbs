ee4bbcbc148f4e4a1d9a85fc8d9588ae7da07d86
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-21 02:15:24 -0200

    Implementa esboço do roteador, já com capacidade de realizar requisições

--- components/controller/src/main/java/controller/net/Router.java
+++ components/controller/src/main/java/controller/net/Router.java
@@ -5,9 +5,35 @@ package controller;
    Julia G. C. Chiba (@JuliaChiba)      - 20511823
  */
 
+import java.net.Socket;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 class Router
 {
     public Router()
     {
+    }
+
+    public String makeRequest(String host, int port, String payload)
+    {
+        try(Socket socket = new Socket(host, port);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream())))
+        {
+            bw.write(payload);
+            bw.write("\n");
+            bw.flush();
+
+            return br.readLine();
+        }
+        catch(IOException e)
+        {
+            return null;
+        }
     }
 }
