6fbecb244a5bf47d3bf64bfaeb986dcd173b3131
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-21 02:14:34 -0200

    Implementa modelo de requisição do controlador

--- components/controller/src/main/java/controller/model/Request.java
+++ components/controller/src/main/java/controller/model/Request.java
@@ -5,9 +5,25 @@ package controller;
    Julia G. C. Chiba (@JuliaChiba)      - 20511823
  */
 
+import java.util.HashMap;
+
 class Request
 {
+    private String kind;
+    private HashMap<String, String> envelope;
+
     public Request()
     {
+        this.envelope = new HashMap<>();
+    }
+
+    public void setKind(String kind)
+    {
+        this.kind = kind;
+    }
+
+    public void setSender(String sender)
+    {
+        this.envelope.put("from", sender);
     }
 }
