bc09d8e45e3fd92fbc56e11b3f9e1575cf4d9fac
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-21 02:14:58 -0200

    Implementa parser de respostas do servidor

--- components/controller/src/main/java/controller/dialog/Parser.java
+++ components/controller/src/main/java/controller/dialog/Parser.java
@@ -5,9 +5,36 @@ package controller;
    Julia G. C. Chiba (@JuliaChiba)      - 20511823
  */
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 class Parser
 {
-    public Parser()
+    private Gson gson;
+    private JsonObject content;
+
+    public Parser(String content)
     {
+        this.gson = new Gson();
+        this.content = gson.fromJson(content, JsonObject.class);
+    }
+
+    private String getKeyAsString(JsonObject obj, String key)
+    {
+        return obj.get(key).getAsString();
+    }
+
+    private JsonObject getKeyAsObject(JsonObject obj, String key)
+    {
+        return obj.get(key).getAsJsonObject();
+    }
+    private JsonObject getEnvelope()
+    {
+        return getKeyAsObject(content, "envelope");
+    }
+
+    public String getSender()
+    {
+        return getKeyAsString(getEnvelope(), "from");
     }
 }
