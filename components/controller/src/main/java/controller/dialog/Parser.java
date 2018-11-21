66646dbdfbbbc5ab21a47a75582efaec1cc687fe
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-21 00:55:31 -0200

    Inclui parser e modelo de requisição não implementados

---
+++ components/controller/src/main/java/controller/dialog/Parser.java
@@ -0,0 +1,13 @@
+package controller;
+/*
+   Caian R. Ertl     (@caianrais)       - 20733152
+   Diogo Casagrande  (@DiogoCasagrande) - 20718678
+   Julia G. C. Chiba (@JuliaChiba)      - 20511823
+ */
+
+class Parser
+{
+    public Parser()
+    {
+    }
+}
