0bc58949ff2c0b32a6070ecdccfa9a0b6c6435d0
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-22 03:59:55 -0200

    Inclui classe não implementada de thread que lida com conexão

---
+++ components/controller/src/main/java/controller/net/Connection.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.net.Socket;
+
+class Connection implements Runnable
+{
+    public Connection(Socket client, Router router)
+    {
+
+    }
+
+    public void run()
+    {
+
+    }
+}
