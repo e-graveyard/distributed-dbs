8737c7df1ce31cc3bb74a794471033cef58325a0
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-22 16:03:11 -0200

    Faz verificação de erro na repetição, inclui logs

--- components/controller/src/main/java/controller/task/Retry.java
+++ components/controller/src/main/java/controller/task/Retry.java
@@ -12,26 +12,28 @@ class Retry implements Runnable
     private Router router;
     private int clock;
 
-    public Retry(Router router, int secondsUntilChecking)
+    public Retry(Router router, int clock)
     {
         this.router = router;
-        this.clock  = secondsUntilChecking * 1000;
+        this.clock = clock;
     }
 
     public void run()
     {
-        boolean active = true;
-        try
+        while(true)
         {
-            while(active)
+            try
             {
-                Thread.sleep(clock);
-                router.checkServersAvailability();
+                Thread.sleep(clock * 1000);
             }
-        }
-        catch(InterruptedException e)
-        {
-            active = false;
+            catch(InterruptedException e)
+            {
+                break;
+            }
+
+            Logger.info("Checking servers availability");
+            router.checkServersAvailability();
+            Logger.info("Cheking again in @secs seconds.".replace("@secs", Integer.toString(clock)));
         }
     }
 }
