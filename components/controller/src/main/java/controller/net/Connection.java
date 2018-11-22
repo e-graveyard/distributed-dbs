950abfaa1c5cf04d2fc5a776e3f01a7cf5292b1e
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-11-22 06:02:40 -0200

    Inclui atributos, implementa início do fluxo de roteamento de mensagens

--- components/controller/src/main/java/controller/net/Connection.java
+++ components/controller/src/main/java/controller/net/Connection.java
@@ -1,16 +1,31 @@
 package controller;
+/*
+   Caian R. Ertl     (@caianrais)       - 20733152
+   Diogo Casagrande  (@DiogoCasagrande) - 20718678
+   Julia G. C. Chiba (@JuliaChiba)      - 20511823
+ */
 
 import java.net.Socket;
 
 class Connection implements Runnable
 {
+    private Socket client;
+    private Router router;
+
     public Connection(Socket client, Router router)
     {
-
+        this.client = client;
+        this.router = router;
     }
 
     public void run()
     {
+        String clientRequest = router.request(client, null),
+               requestKind   = (new Parser(clientRequest)).getKind();
 
+        if(!router.areAllServersAvailable && requestKind != "ReadRecord")
+        {
+            // impossible
+        }
     }
 }
